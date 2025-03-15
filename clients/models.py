4bdb116bc037a0eea76db6dbda5ed9319f897b69
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-03-15 23:54:18 +0600

    push

--- clients/models.py
+++ clients/models.py
@@ -17,14 +17,17 @@ class Service(models.Model):
         return self.name
 
 
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 class Client(models.Model):
     full_name = models.CharField(max_length=255)
     appointment_day = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(31)])
     appointment_month = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(12)])
-    appointment_time = models.TimeField()  # Добавляем время
+    appointment_time = models.TimeField(default="12:00")  # Указываем значение по умолчанию
     phone_number = models.CharField(max_length=20)
-    services = models.ManyToManyField(Service)
-    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    services = models.ManyToManyField("Service")
+    worker = models.ForeignKey("Worker", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.full_name} — {self.appointment_day}/{self.appointment_month} {self.appointment_time.strftime('%H:%M')}"
