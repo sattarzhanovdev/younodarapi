2f22ffe54c8b4fe7c59a3b551753be8d1ccb9c73
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-03-16 00:40:04 +0600

    push

--- clients/admin.py
+++ clients/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Client, Service, Worker
+from .models import Service, Worker, WorkerServiceShare, WorkerServiceShareDetail
 
-admin.site.register(Client)
+class WorkerServiceShareDetailInline(admin.TabularInline):  # Позволяет добавлять услуги на одной странице
+    model = WorkerServiceShareDetail
+    extra = 1  # Показывать 1 пустую строку для добавления новой услуги
+
+class WorkerServiceShareAdmin(admin.ModelAdmin):
+    list_display = ('worker',)
+    inlines = [WorkerServiceShareDetailInline]  # Подключаем inline
+
 admin.site.register(Service)
 admin.site.register(Worker)
+admin.site.register(WorkerServiceShare, WorkerServiceShareAdmin)
--- clients/models.py
+++ clients/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 
 class Worker(models.Model):
     name = models.CharField(max_length=255)
@@ -16,6 +17,39 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class WorkerServiceShare(models.Model):
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    service = models.ManyToManyField(Service, through='WorkerServiceShareDetail')
+
+    def __str__(self):
+        return f"{self.worker}"
+
+class WorkerServiceShareDetail(models.Model):
+    worker_service_share = models.ForeignKey(WorkerServiceShare, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    share_type = models.CharField(
+        max_length=10,
+        choices=[('percentage', 'Процент'), ('fixed', 'Фиксированная сумма')],
+        default='percentage'
+    )
+    percentage = models.FloatField(null=True, blank=True)
+    fixed_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def clean(self):
+        if self.share_type == 'percentage' and self.percentage is None:
+            raise ValidationError("Если выбрано 'Процент', поле 'percentage' обязательно.")
+        if self.share_type == 'fixed' and self.fixed_amount is None:
+            raise ValidationError("Если выбрана 'Фиксированная сумма', поле 'fixed_amount' обязательно.")
+        if self.share_type == 'percentage' and self.fixed_amount is not None:
+            raise ValidationError("При выборе 'Процент' поле 'fixed_amount' должно быть пустым.")
+        if self.share_type == 'fixed' and self.percentage is not None:
+            raise ValidationError("При выборе 'Фиксированная сумма' поле 'percentage' должно быть пустым.")
+
+    def __str__(self):
+        return f"{self.worker_service_share.worker} - {self.service} ({self.share_type})"
 
 class Client(models.Model):
     full_name = models.CharField(max_length=255)
