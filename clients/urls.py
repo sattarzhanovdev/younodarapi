4b08b12671d6649abcdcb35112b573d42289cdc8
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-04-05 02:57:18 +0600

    push

--- clients/urls.py
+++ clients/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WorkerViewSet, ServiceViewSet, ClientViewSet, ExpenseListCreateView, ExpenseDetailView, MonthlyExpensesView, DailyStatsView, ClientsAddedTodayView
+from .views import WorkerViewSet, ServiceViewSet, ClientViewSet, ExpenseListCreateView, ExpenseDetailView, MonthlyExpensesView, ExpenseStatsView, ClientsAddedTodayView
 
 router = DefaultRouter()
 router.register(r'workers', WorkerViewSet)
@@ -13,6 +13,6 @@ urlpatterns = [
     path('expenses/', ExpenseListCreateView.as_view(), name='expense-list-create'),
     path('expenses/<int:pk>/', ExpenseDetailView.as_view(), name='expense-detail'),
     path('expenses/monthly/<int:year>/<int:month>/', MonthlyExpensesView.as_view(), name='monthly-expenses'),
-    path('expenses/stats/daily/', DailyStatsView.as_view(), name='daily-expense-stats'),
+    path('expenses/stats/daily/', ExpenseStatsView.as_view(), name='daily-expense-stats'),
     path('today/', ClientsAddedTodayView.as_view(), name='clients-today')
 ]
