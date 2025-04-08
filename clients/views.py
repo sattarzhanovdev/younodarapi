6cdd88c91dfd43ac41808643c2b59446f319d408
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-04-08 13:50:20 +0600

    push

--- clients/views.py
+++ clients/views.py
@@ -19,6 +19,21 @@ class StockViewSet(viewsets.ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
 
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        # Если отправлен список
+        if isinstance(data, list):
+            serializer = self.get_serializer(data=data, many=True)
+        else:
+            serializer = self.get_serializer(data=data)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 class CabinetsViewSet(viewsets.ModelViewSet):
     queryset = Cabinets.objects.all()
     serializer_class = CabinetsSerializer
